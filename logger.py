5561698f3aae7fd9437362689a0da4c91b2634f3
Pierre Julien <pierrejulienpro@gmail.com>
2021-01-03 19:18:35 +0100

    Ajout d'une gestion de l'épuration du fichier de log

--- logger.py
+++ logger.py
@@ -1,11 +1,17 @@
 import logging
+import os
+import time
 
 class Logger:
+    filename='discord.log'
+    date_format='%Y-%m-%d %H:%M'
+
     def __init__(self):
-        logging.basicConfig(filename='discord.log', level=logging.INFO)
+        if os.path.exists(self.filename): os.remove(self.filename)
+        logging.basicConfig(filename=self.filename, level=logging.INFO)
  
     def i(self, message=''):
-        logging.info(message)
+        logging.info(f'[{time.strftime(self.date_format)}] {message}')
 
     def e(self, error):
-        logging.error(error)
+        logging.error(f'[{time.strftime(self.date_format)}] {error}')
