820368a40e20ee632cd7be4b639ebcba1284b703
Pierre Julien <pierrejulienpro@gmail.com>
2021-01-25 22:26:13 +0100

    Ajout d'une commande ping et une commande permettant la récupération du fichier de log

--- discord_bot.py
+++ discord_bot.py
@@ -1,22 +1,33 @@
 import config
-import discord
-from discord.ext import tasks
-from datetime import datetime
-import discord_helper
 import logger
+import discord
+import discord_helper
+from discord.ext import tasks, commands
+from datetime import datetime
 
 log = logger.Logger()
-client = discord.Client()
+client = commands.Bot(command_prefix='$')
 discord_helper = discord_helper.DiscordHelper(client)
 
 @client.event
 async def on_ready():
     log.i(f'{client.user} is connected to Discord!')
-
-    if config.is_debug: 
-        await discord_helper.send_reddit_submissions_to_discord()
-        await client.close()
+    if config.is_debug: await discord_helper.send_reddit_submissions_to_discord()
     else: reddit_submissions_task.start()
+
+@client.command()
+async def ping(ctx):
+    latency = round(client.latency * 1000)
+    author = f'{ctx.author.name}#{ctx.author.discriminator}'
+    ping = f'Ping : {latency}ms.'
+    log.i(f'{author} ask for latency. {ping}')
+    await ctx.send(ping)
+
+@client.command(aliases=['log'])
+async def get_log(ctx):
+    author = f'{ctx.author.name}#{ctx.author.discriminator}'
+    log.i(f'{author} ask for log file.')
+    await ctx.send(file=discord.File(log.filename))
 
 @tasks.loop(minutes=1)
 async def reddit_submissions_task():
