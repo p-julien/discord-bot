6c7e0ecc9e343b701159e4d0de376539431310bf
Pierre Julien <pierrejulienpro@gmail.com>
2021-02-07 16:52:22 +0100

    Ajout de la commande pull + refactoring des logs debug du bot

--- discord_bot.py
+++ discord_bot.py
@@ -12,7 +12,7 @@ discord_helper = discord_helper.DiscordHelper(client)
 
 @client.event
 async def on_ready():
-    log.i(f'{client.user} is connected to Discord!')
+    log.i(f'✔️ {client.user} is connected to Discord!')
     if config.is_debug: 
         await discord_helper.send_reddit_submissions_to_discord()
         return
@@ -22,27 +22,27 @@ async def on_ready():
 
 @client.event
 async def on_command_error(context, exception):
-    log.e("An error occured while restarting the submissions command to Discord.", exception)
+    log.e("❌ An error occured while restarting the submissions command to Discord.", exception)
     await context.send(str(exception))
 
 @client.command()
 async def ping(ctx):
     latency = round(client.latency * 1000)
     author = f'{ctx.author.name}#{ctx.author.discriminator}'
-    log.i(f'{author} ask for latency: {latency}ms')
+    log.i(f'ℹ️ {author} ask for latency: {latency}ms')
     await ctx.send(f'Latency : {latency}ms')
 
 @client.command(aliases=['log'])
 async def get_log(ctx):
     author = f'{ctx.author.name}#{ctx.author.discriminator}'
-    log.i(f'{author} ask for log file.')
+    log.i(f'ℹ️ {author} ask for log file.')
     await ctx.send(file=discord.File(log.filename))
 
 @client.command()
 @commands.cooldown(1, 30)
 async def restart(ctx):
     author = f'{ctx.author.name}#{ctx.author.discriminator}'
-    log.i(f'{author} wants to restart the reddit submissions.')
+    log.i(f'ℹ️ {author} wants to restart the reddit submissions.')
     await discord_helper.send_reddit_submissions_to_discord()
 
 @client.command()
@@ -50,8 +50,15 @@ async def advice(ctx):
     author = f'{ctx.author.name}#{ctx.author.discriminator}'
     r = requests.get('https://api.adviceslip.com/advice').json()
     advice = r['slip']['advice']
-    log.i(f'{author} ask for an advice. {advice}')
+    log.i(f'ℹ️ {author} ask for an advice. {advice}')
     await ctx.send(advice)
+
+@client.command()
+@commands.cooldown(5, 30)
+async def pull(ctx):
+    author = f'{ctx.author.name}#{ctx.author.discriminator}'
+    log.i(f'ℹ️ {author} ask for a pull on channel {ctx.channel.name}')
+    await discord_helper.send_reddit_submissions_to_discord_channel(ctx.channel)
 
 @tasks.loop(minutes=1)
 async def reddit_submissions_task():
--- discord_helper.py
+++ discord_helper.py
@@ -1,6 +1,6 @@
-import discord
 import config
 import logger
+import discord
 import reddit_helper
 import burger_express
 import decode_url
@@ -19,51 +19,65 @@ class DiscordHelper:
         self.client = client
 
     async def send_as_attachment_on_discord_channel(self, discord_channel, submission, mime_type, content):
-        self.log.i(f"Attachment: {content}")
+        self.log.i(f"🖼️ Attachment: {submission.title}")
 
         filename = f"{submission}.{mime_type[1]}" if not submission.over_18 else f"SPOILER_{submission}.{mime_type[1]}"
         filepath = os.path.join("temp", filename)
 
         fo = open(filepath, 'wb')
         fo.write(requests.get(submission.url, allow_redirects=True).content)
-        submission_file = discord.File(filepath, filename)
         fo.close()
 
         if config.is_debug: return
+
+        submission_file = discord.File(filepath, filename)
         await discord_channel.send(content=content, file=submission_file)
 
     async def send_as_message_on_discord_channel(self, discord_channel, submission, content):
         content = f"{content}\n{submission.url}"
-        self.log.i(f"Message: {content}")
+        self.log.i(f"📝 Message: {submission.title}")
 
         if config.is_debug: return
         await discord_channel.send(content)
 
+    def is_attachment(self, submission, mime_type):
+        return mime_type[0] == "image" and int(self.decode_url.get_file_size_from_url(submission.url)) < 8000000
+
     async def send_reddit_submission_on_discord_channel(self, discord_channel, submission):
-        content = f"**{submission.title}**" if not submission.over_18 else f"🔞 **{submission.title}** 🔞"
-        submission_url_mime_type = self.decode_url.get_mime_type_from_url(submission.url).split("/")
+        try:
+            content = f"**{submission.title}**" if not submission.over_18 else f"🔞 **{submission.title}** 🔞"
+            submission_url_mime_type = self.decode_url.get_mime_type_from_url(submission.url).split("/") 
 
-        if (submission_url_mime_type[0] == "image" and int(self.decode_url.get_file_size_from_url(submission.url)) < 8000000):
-            await self.send_as_attachment_on_discord_channel(discord_channel, submission, submission_url_mime_type, content)
-        else:
-            await self.send_as_message_on_discord_channel(discord_channel, submission, content)
+            if self.is_attachment(submission, submission_url_mime_type):
+                await self.send_as_attachment_on_discord_channel(discord_channel, submission, submission_url_mime_type, content)
+            else:
+                await self.send_as_message_on_discord_channel(discord_channel, submission, content)
 
+        except Exception as ex:
+            self.log.e(f"❌ An error occured while sending submission:{submission} to Discord.", ex)
 
     async def send_reddit_submissions_to_discord(self):
         try:
-            self.log.i("Start sending submissions on Discord!")
+            self.log.i("✔️ Start sending submissions on Discord!")
             if not os.path.isdir('temp'): os.mkdir("temp")
                 
             for discord_channel in self.client.get_all_channels():
-
                 if not self.burger_express.is_sub_reddit_correspondence(discord_channel.name): continue
-                sub_reddit = self.burger_express.get_sub_reddit_name_by_channel_discord(discord_channel.name)
-
-                self.log.i(f"Posts from {sub_reddit} in channel {discord_channel.name}:")
-                for submission in self.reddit_helper.get_posts_by_sub_reddit(sub_reddit):
-                    await self.send_reddit_submission_on_discord_channel(discord_channel, submission)
+                await self.send_reddit_submissions_to_discord_channel(discord_channel)
 
             shutil.rmtree("temp")
-            self.log.i("End of submissions sending!")
+            self.log.i("✔️ End of submissions sending!")
         except Exception as ex:
-            self.log.e("An error occured while sending submissions to Discord.", ex)
+            self.log.e("❌ An error occured while sending submissions to Discord.", ex)
+
+    async def send_reddit_submissions_to_discord_channel(self, discord_channel):
+        try:
+            if not self.burger_express.is_sub_reddit_correspondence(discord_channel.name): return
+            sub_reddit = self.burger_express.get_sub_reddit_name_by_channel_discord(discord_channel.name) 
+            
+            self.log.i(f"ℹ️ Posts from {sub_reddit} in channel {discord_channel.name}:")
+            for submission in self.reddit_helper.get_posts_by_sub_reddit(sub_reddit):
+                await self.send_reddit_submission_on_discord_channel(discord_channel, submission)
+
+        except Exception as ex:
+            self.log.e(f"❌ An error occured while sending submissions to Discord channel {discord_channel.name}.", ex)
