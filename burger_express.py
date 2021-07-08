dc33fc5891aac33434ad11fab991cedb13310b07
Pierre Julien <pierrejulienpro@gmail.com>
2021-07-08 11:03:01 +0200

    Correction de bug sur la récupération du reddit associé à un salon

--- burger_express.py
+++ burger_express.py
@@ -33,7 +33,7 @@ class BurgerExpress:
     def get_sub_reddit_name_by_channel_discord(self, discord_channel_name):
         for element in self.discord_channels_sub_reddits:
             
-            if (isinstance(element[1], list)):
+            if (element[0] == discord_channel_name and isinstance(element[1], list)):
                 multi_sub_reddit = ""
                 for sub_reddit in element[1]:
                     multi_sub_reddit += sub_reddit + "+"
