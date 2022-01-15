1f7ccdc6487b82beb2ba7b4965917c7fd3c85cb9
Pierre Julien <pierrejulienpro@gmail.com>
2022-01-15 22:12:10 +0100

    Corriger récupération de la position de Maxime sur la commande /whereisdoche

--- src/commands/doche-location-command.js
+++ src/commands/doche-location-command.js
@@ -1,35 +1,37 @@
-import {MessageEmbed} from 'discord.js';
-import fetch from 'node-fetch';
+import { MessageEmbed } from "discord.js";
+import fetch from "node-fetch";
 
 export class WhereIsDocheCommand {
-    
-    constructor(client, interaction) {
-        this.client = client;
-        this.interaction = interaction;
-    }
+  constructor(client, interaction) {
+    this.client = client;
+    this.interaction = interaction;
+  }
 
-    async run() {
-        this.client.api.interactions(this.interaction.id, this.interaction.token)
-            .callback
-            .post({
-                data: {
-                    type: 4,
-                    data: {
-                        embeds: [ await this.getEmbedMessage() ]
-                    }
-                }
-            })
-    }
+  async run() {
+    this.client.api
+      .interactions(this.interaction.id, this.interaction.token)
+      .callback.post({
+        data: {
+          type: 4,
+          data: {
+            embeds: [await this.getEmbedMessage()],
+          },
+        },
+      });
+  }
 
-    async getEmbedMessage() {
-        const docheLocation = await this.getDocheLocation()
-        return new MessageEmbed()
-            .setColor('#E6742B')
-            .setTitle(`🛰️ Doche location: ${docheLocation}`)
-    }
+  async getEmbedMessage() {
+    const docheLocation = await this.getDocheLocation();
+    return new MessageEmbed()
+      .setColor("#E6742B")
+      .setTitle(`🛰️ Doche location: ${docheLocation}`);
+  }
 
-    async getDocheLocation() {
-        const response  = await fetch('http://codem.tk/ou-suis-je')
-        return await response.text()
-    }
+  async getDocheLocation() {
+    const response = await fetch(
+      "https://codem.tk/geo/api/discord-user/192718089979166720"
+    );
+    const doche = await response.json();
+    return await doche.geolocalisation;
+  }
 }
