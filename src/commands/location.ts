76cfc4fb2ae5efbcecb43a92a59edeb24cd4bfc7
Pierre Julien <pierrejulienpro@gmail.com>
2022-02-12 21:01:11 +0100

    Evolution de la commande de localisation pour récupérer les informations géographique de n'importe quel utilisateur

--- src/commands/location.ts
+++ src/commands/location.ts
@@ -1,31 +1,49 @@
-import { BaseCommandInteraction, Client, MessageEmbed } from "discord.js";
+import {
+    ApplicationCommandAutocompleteOption,
+    BaseCommandInteraction,
+    Client,
+    MessageEmbed,
+} from "discord.js";
 import { Command } from "./command.interface";
 import fetch from "node-fetch";
+import { ApplicationCommandOptionTypes } from "discord.js/typings/enums";
 
 export class Location implements Command {
     name = "location";
     description = "Get the localisation of an user";
+    options = new Array<ApplicationCommandAutocompleteOption>({
+        type: ApplicationCommandOptionTypes.STRING,
+        autocomplete: true,
+        name: "user_id",
+        required: true,
+        description: "Id of the user",
+    });
 
     async run(client: Client, interaction: BaseCommandInteraction) {
-        const embed = await this.getEmbed();
+        const parameters = interaction.options.data as any;
+        const userId = parameters.find((p: any) => p.name === "user_id").value;
+        const location = await this.getLocation(userId);
+        const embed = await this.getEmbed({
+            userId: userId,
+            location: location,
+        });
+
         await interaction.followUp({
             ephemeral: true,
             embeds: [embed],
         });
     }
 
-    private async getEmbed() {
-        const docheLocation = await this.getDocheLocation();
+    private async getEmbed(data: any) {
         return new MessageEmbed()
             .setColor("#E6742B")
-            .setTitle(`🛰️ Doche location: ${docheLocation}`);
+            .setTitle(`🛰️ Location ${data.userId}: ${data.location}`);
     }
 
-    private async getDocheLocation() {
-        const response = await fetch(
-            "https://codem.tk/geo/api/discord-user/192718089979166720"
-        );
+    private async getLocation(userId: string) {
+        const uri = `https://codem.tk/geo/api/discord-user/${userId}`;
+        const response = await fetch(uri);
         const data = (await response.json()) as any;
-        return data.geolocalisation;
+        return data.geolocalisation as string;
     }
 }
